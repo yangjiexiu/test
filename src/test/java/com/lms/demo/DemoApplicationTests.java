0722d5a956587d86cc6554ef3c9285a947c4e6d6
yjx <754148147@qq.com>
2023-11-17 15:56:47 +0800

    添加发邮件功能

--- src/test/java/com/lms/demo/DemoApplicationTests.java
+++ src/test/java/com/lms/demo/DemoApplicationTests.java
@@ -3,15 +3,41 @@ package com.lms.demo;
 import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootTest
 @Slf4j
 class DemoApplicationTests {
+    @Autowired
+    JavaMailSender sender;
+    @Value("${spring.mail.username}")
+    String mailName;
 
     @Test
     void contextLoads() {
         log.info("testinfo");
     }
+
+
+    @Test
+    void testMail() {
+        //SimpleMailMessage是一个比较简易的邮件封装，支持设置一些比较简单内容
+        SimpleMailMessage message = new SimpleMailMessage();
+        //设置邮件标题
+        message.setSubject("【标题】1");
+        //设置邮件内容
+        message.setText("1111");
+        //设置邮件发送给谁，可以多个，这里就发给你的QQ邮箱
+        message.setTo("754148147@qq.com");
+        //邮件发送者，这里要与配置文件中的保持一致
+        message.setFrom(mailName);
+        //OK，万事俱备只欠发送
+        sender.send(message);
+    }
+
 
 }
